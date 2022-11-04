68fa1fb0cac0b18dd70ac0345d28657fd4969616
한규 <65541546+VENDREDl@users.noreply.github.com>
2022-11-04 17:27:11 +0900

    Update __init__.py

--- Flask/views/__init__.py
+++ Flask/views/__init__.py
@@ -3,18 +3,9 @@ import logging
 
 from . import main
 
-from filter import DatetimeStrForm, Perm, UserStrForm
-
 app = Flask(__name__)
 
 log = logging.getLogger('werkzeug')
 log.disabled = True
-
-app.jinja_env.filters["datetimefh"] = DatetimeStrForm.cvtDatetimeFormDeltaH
-app.jinja_env.filters["datetimef"] = DatetimeStrForm.cvtDatetimeForm
-app.jinja_env.filters["hasAnaly"] = Perm.hasAnaly
-app.jinja_env.filters["hasAdmin"] = Perm.hasAdmin
-app.jinja_env.filters["boolSimb"] = Perm.cvtBoolSimb
-app.jinja_env.filters["genderStr"] = UserStrForm.cvtGenderStr
 
 app.register_blueprint(main.bp)
